c24f0625eaf2b0b8cc0c86eb1e326eab6f39e1a6
vamshi <vamshiprasannapotla28@gmail.com>
2026-02-21 13:45:03 +0530

    one

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,7 @@
 import { auth } from "./firebase.js";
 import {
 createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-sendEmailVerification,
-onAuthStateChanged,
-signOut
+signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const loginForm = document.getElementById("loginForm");
@@ -18,9 +15,8 @@ const email = signupForm.querySelector("input[type='email']").value;
 const password = signupForm.querySelector("input[type='password']").value;
 
 try {
-const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-await sendEmailVerification(userCredential.user);
-alert("Verification email sent. Please verify before login.");
+await createUserWithEmailAndPassword(auth, email, password);
+alert("Account created successfully! You can now log in.");
 } catch (error) {
 alert(error.message);
 }
@@ -34,17 +30,9 @@ const email = loginForm.querySelector("input[type='email']").value;
 const password = loginForm.querySelector("input[type='password']").value;
 
 try {
-const userCredential = await signInWithEmailAndPassword(auth, email, password);
-
-if (!userCredential.user.emailVerified) {
-alert("Please verify your email before logging in.");
-await signOut(auth);
-return;
-}
-
+await signInWithEmailAndPassword(auth, email, password);
 alert("Login successful!");
 window.location.href = "index.html";
-
 } catch (error) {
 alert(error.message);
 }
--- js/firebase.js
+++ js/firebase.js
@@ -5,14 +5,15 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // Paste YOUR config here
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAVztU4PzBMVfLK7aqreJPjuQ_a4hzEtys",
-  authDomain: "miniing-website-2807.firebaseapp.com",
-  projectId: "miniing-website-2807",
-  storageBucket: "miniing-website-2807.firebasestorage.app",
-  messagingSenderId: "999934720353",
-  appId: "1:999934720353:web:a6d131032f2cca1707ed20",
-  measurementId: "G-5SRRVPJCS2"
+  apiKey: "AIzaSyDpOAscZrpFMhich8xDFQw0pv_27Y_qkZ8",
+  authDomain: "vamshi-one.firebaseapp.com",
+  projectId: "vamshi-one",
+  storageBucket: "vamshi-one.firebasestorage.app",
+  messagingSenderId: "219596891116",
+  appId: "1:219596891116:web:e92f700c47be8dddbd96c9",
+  measurementId: "G-R7X7TGV15C"
 };
 
 // Initialize Firebase
